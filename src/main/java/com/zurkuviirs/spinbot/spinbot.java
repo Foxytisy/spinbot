9b69b3c5402058e4c4331ed2651675823bb077b0
Malaclord <56694430+Malaclord@users.noreply.github.com>
2024-08-25 23:35:43 +0200

    use mixin inject in client render method for smooth spin

--- src/main/java/com/zurkuviirs/spinbot/spinbot.java
+++ src/main/java/com/zurkuviirs/spinbot/spinbot.java
@@ -4,17 +4,9 @@ package com.zurkuviirs.spinbot;
 
 
 import com.mojang.brigadier.arguments.FloatArgumentType;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.player.*;
-import net.minecraft.text.Text;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
@@ -25,7 +17,15 @@ public class spinbot implements ClientModInitializer {
     public float spinAmount = 0;
     //public float spinAngle = 0;
 
-    public void onInitializeClient(){
+    private static spinbot instance;
+
+    public static spinbot getInstance() {
+        return instance;
+    }
+
+    public void onInitializeClient() {
+        instance = this;
+
         ClientCommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess) -> dispatcher.register(literal("spin")
                 .then(ClientCommandManager.argument("How much spin?", FloatArgumentType.floatArg()).executes(context -> {
                     spinEnable = true;
@@ -59,15 +59,6 @@ public class spinbot implements ClientModInitializer {
             //angleSpinEnable = false;
             return 1;
         }))));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (spinEnable && client.player != null) {
-                // Calculate the amount to rotate each tick
-                float increment = spinAmount / 20.0f; // Spread the rotation over multiple ticks
-                client.player.setYaw(client.player.getYaw() + increment);
-                //client.player.sendMessage(Text.literal(String.valueOf(client.player.getYaw())));
-            }
-        });
 
        //ClientTickEvents.END_CLIENT_TICK.register(client -> {
        //     if (angleSpinEnable && client.player != null) {
