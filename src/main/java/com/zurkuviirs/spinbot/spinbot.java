23ef507fa9862cd516c908fcd83abb1b61a6510d
Martins <m@treiman.s>
2024-08-25 23:28:51 +0300

    It starts here :O

--- src/main/java/com/zurkuviirs/spinbot/spinbot.java
+++ src/main/java/com/zurkuviirs/spinbot/spinbot.java
@@ -21,9 +21,9 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.lit
 public class spinbot implements ClientModInitializer {
 
     public boolean spinEnable = false;
-    public boolean angleSpinEnable = false;
+    //public boolean angleSpinEnable = false;
     public float spinAmount = 0;
-    public float spinAngle = 0;
+    //public float spinAngle = 0;
 
     public void onInitializeClient(){
         ClientCommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess) -> dispatcher.register(literal("spin")
@@ -39,24 +39,24 @@ public class spinbot implements ClientModInitializer {
         })))))
         ;
 
-        ClientCommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess) -> dispatcher.register(literal("spinangle")
-                .then(ClientCommandManager.argument("Angle?", FloatArgumentType.floatArg())
-                        .then(ClientCommandManager.argument("How much spin?", FloatArgumentType.floatArg())).executes(context -> {
-                    angleSpinEnable = true;
-                    spinAmount = FloatArgumentType.getFloat(context, "How much spin?") / 20.0f;
-                    spinAngle = FloatArgumentType.getFloat(context, "Angle?");
-                    //ClientPlayerEntity player = context.getSource().getPlayer();
-                    //assert player != null;
-                    //player.setYaw(1);
-                    //System.out.println(player.getYaw());
-
-                    return 1;
-                })))))
-        ;
+        //ClientCommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess) -> dispatcher.register(literal("spinangle")
+        //        .then(ClientCommandManager.argument("Angle?", FloatArgumentType.floatArg())
+        //                .then(ClientCommandManager.argument("How much spin?", FloatArgumentType.floatArg())).executes(context -> {
+        //            angleSpinEnable = true;
+        //            spinAmount = FloatArgumentType.getFloat(context, "How much spin?") / 20.0f;
+        //            spinAngle = FloatArgumentType.getFloat(context, "Angle?");
+        //            //ClientPlayerEntity player = context.getSource().getPlayer();
+        //            //assert player != null;
+        //            //player.setYaw(1);
+        //            //System.out.println(player.getYaw());
+//
+        //            return 1;
+        //        })))))
+        //;
 
         ClientCommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> dispatcher.register(literal("spinstop").executes(context -> {
             spinEnable = false;
-            angleSpinEnable = false;
+            //angleSpinEnable = false;
             return 1;
         }))));
 
@@ -65,17 +65,17 @@ public class spinbot implements ClientModInitializer {
                 // Calculate the amount to rotate each tick
                 float increment = spinAmount / 20.0f; // Spread the rotation over multiple ticks
                 client.player.setYaw(client.player.getYaw() + increment);
-                client.player.sendMessage(Text.literal(String.valueOf(client.player.getYaw())));
+                //client.player.sendMessage(Text.literal(String.valueOf(client.player.getYaw())));
             }
         });
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (angleSpinEnable && client.player != null) {
-                // Calculate the amount to rotate each tick
-                float increment = spinAmount / 20.0f; // Spread the rotation over multiple ticks
-                client.player.setYaw(client.player.getYaw() + increment);
-            }
-        });
+       //ClientTickEvents.END_CLIENT_TICK.register(client -> {
+       //     if (angleSpinEnable && client.player != null) {
+       //         // Calculate the amount to rotate each tick
+       //         float increment = spinAmount / 20.0f; // Spread the rotation over multiple ticks
+        //        client.player.setYaw(client.player.getYaw() + increment);
+           // }
+        //});
     }
 
 }
